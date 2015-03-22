799fc370636efe383dda0fbfed41b11dc60dbcca
warbaby <warbaby@163.com>
2015-03-23 00:18:09 +0800

    test

---
+++ rest-documentation-example/src/main/java/warbaby/test/AddressLombok.java
@@ -0,0 +1,16 @@
+package warbaby.test;
+
+import lombok.Data;
+
+/**
+ * @author WangBei
+ * @since 2015/3/22
+ */
+@Data
+public class AddressLombok {
+    /** ID2的说明 */
+    private int id2;
+    /** 街道 */
+    private String street2;
+    private String building2;
+}
